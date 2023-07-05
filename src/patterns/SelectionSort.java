0e25b032569e8742e25bd3d3c8f1acc4c576c758
vinayharyan28 <vinayharyan28@gmail.com>
2023-07-05 22:23:59 +0530

    add selection sort & mergesort

--- src/patterns/SelectionSort.java
+++ src/patterns/SelectionSort.java
@@ -1,11 +1,30 @@
 package patterns;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args){
-
+        int[] arr = {1, 10, 2, 33, 4};
+        selectionSort(arr, arr.length, 0, 0);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void selectionSort(int[] arr, int r, int c, int max){
+    public static void selectionSort(int[] arr, int row, int column, int max){
+        if (row == 0){
+            return;
+        }
 
+        if (column < row){
+            if (arr[column] > arr[max]){
+                selectionSort(arr, row, column+1, column);
+            }else {
+                selectionSort(arr, row, column+1, max);
+            }
+        }else {
+            int temp = arr[max];
+            arr[max] = arr[row-1];
+            arr[row-1] = temp;
+            selectionSort(arr, row-1, 0, 0);
+        }
     }
 }
---
+++ src/sorting/MergeSort.java
@@ -0,0 +1,54 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 9, 2, 1, 0};
+        int[] ans = mergeSort(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] mergeSort(int[] arr){
+        if (arr.length == 1){
+            return arr;
+        }
+        int mid = arr.length / 2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+        
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+        int i=0, j=0, k=0;
+
+        while (i < first.length && j < second.length){
+            if (first[i] < second[j]){
+                mix[k] = first[i];
+                i++;
+            }else{
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible one of the array not complete
+        // copy the remaining elements
+        while (i < first.length){
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while (j < second.length){
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        return mix;
+
+    }
+}
